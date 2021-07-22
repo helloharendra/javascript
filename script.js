ad8e6f1d0e9e02bb32eec04e9de5eb7e180f2aad
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-07-22 23:20:05 +0530

    java script outputs

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function myFunction(){
-    document.getElementById('demo').innerHTML='paragraph changed'
+document.getElementById("demo").innerHTML=(5+3);
+document.write(10+3);
+function useDocumentwrite()
+{
+document.write(90+20);
 }
+function useWindowalert(){
+    window.alert("don't click me");
+}
+
+function useAlert(){
+    alert("don't click me");
+}
+function usePrint(){
+    window.print("hello harendra");
+}
+
