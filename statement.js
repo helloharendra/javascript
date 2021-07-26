bd517952ec7bfc192dbf50a5dec5927180462a27
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-07-26 21:01:50 +0530

    JS statements

---
+++ statement.js
@@ -0,0 +1,31 @@
+
+function mySum1()
+{
+let x,y,z; //statement 1
+x=10; //statement 2
+y=20; //statement 3
+z=x+y; //statement 4
+
+
+
+document.write("The sum of x and y is  :"+z);
+
+}
+
+function mySum2()
+{
+
+
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+
+document.write("The sum of a and b is  :"+c);
+
+
+}
+
+
+
+
