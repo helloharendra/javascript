c5893e6f9487fb216fdec8b4978aeb7d77cb1e20
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-07-29 00:25:10 +0530

    Js varriables

---
+++ JsVarriables.js
@@ -0,0 +1,21 @@
+function Varriable1(){
+    x=10;
+    y=20;
+    z=x+y;
+    document.write("The sum of x and y is   :"+z);
+}
+
+function Varriable2(){
+    var a=10;
+    var b=20;
+    var c="the sum is:"
+    sum1=c+a+b;
+    document.write(sum1);
+}
+function Varriable3(){
+    $="my name is   :";
+    fname="harendra   ";
+    lname="prajapati";
+    all1=$+fname+lname;
+    document.write(all1);
+}
