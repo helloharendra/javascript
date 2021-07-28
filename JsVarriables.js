9742577d0270dd84c497870489fb07af7c1715f6
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-07-29 01:32:17 +0530

    Js varriables

--- JsVarriables.js
+++ JsVarriables.js
@@ -6,7 +6,7 @@ function Varriable1(){
 }
 
 function Varriable2(){
-    var a=10;
+    var a=10;       //varriable declared as var may be redeclare 
     var b=20;
     var c="the sum is:"
     sum1=c+a+b;
@@ -19,3 +19,53 @@ function Varriable3(){
     all1=$+fname+lname;
     document.write(all1);
 }
+function Varriable4(){
+   var num=10;
+    {
+      var  num=20;
+    }
+    document.write(num);     /* have not a block scope here outpot will be 20  but whenwe use let keyword to assign
+                                 a varriable it removes this problem.*/
+}
+
+function Varriable5(){
+    let num=10;
+    {
+        let num=20;
+    }
+    document.write(num);  /*here outpot will be 10 have a block scope*/
+
+    }
+     
+     function Varriable6(){
+         const a=100;        //can not be redeclare ,reassigned,have a block scope
+         document.write("The value of a is"+a);
+    }
+     
+    function const1(){
+        // You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+document.write("cars are before adding         :"+cars);
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+document.write("cars are after adding         :"+cars);
+
+    }
+
+    function const2(){
+        // You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+document.write(",this added"+car);
+    }
