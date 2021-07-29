862880b60acb065f2b6fb57311f7ca7270bd1f92
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-07-30 01:28:21 +0530

    Js Arithmatics

---
+++ JsArithmatic.js
@@ -0,0 +1,34 @@
+function arithmaticJs(){
+    let x=(10+20);
+    document.write("add      :"+x)
+
+    let c=(10-20);
+    document.write("minus    :"+c)
+
+    let a=(40+60)*2;
+    document.write("mult       :"+a)
+
+
+    let b=(40+60)/2;
+    document.write("div    :   "+b)
+
+    let y=10;
+        y++;
+    let z=y;
+    document.write("the increment is of 10    :"+z);
+
+    let y1=10;
+    y1--;
+let z1=y1;
+document.write("the decrement is of 10  :"+z1);
+
+let y3=10;
+let z3=y3 ** 3;
+document.write("the exponent is using math.write    :"+z3);
+
+
+let y4=10;
+let z4=Math.pow(y4, 2);
+document.write("the exponent is  using math.pow   :"+z4);
+
+}
