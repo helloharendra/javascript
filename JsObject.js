3b18e013e4bac956c87ef9e6651adfb853da1bbd
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-08-11 01:02:22 +0530

    JS Object

---
+++ JsObject.js
@@ -0,0 +1,10 @@
+function firstObject(){
+    const car = {type:"Fiat", model:"500", color:"white"};
+    //return car.type;
+    document.write("the car type is:"+car.type  +"the car modal is:"+car.model +"the car color is:"+car.color);
+}
+ 
+function secondFunction(){
+    const preson={firstName:"harendra",lastName:"Prajapati",age:"23",sex:"male"};
+    document.write(preson.firstName+"  "+preson.lastName+" is "+preson.age+" years old ");
+}
---
+++ JsObject1.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: "Harendra",
+    lastName: "Prajapati",
+    id: 5566,
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  
+  
