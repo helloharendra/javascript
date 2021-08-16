92dfeef3e584ff365d4996c50c92e5a2c18338d3
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-08-17 01:14:52 +0530

    JS String1

---
+++ JsString.js
@@ -0,0 +1,107 @@
+function myString(){
+   let h1="i am harendra prajapati and i am mca student at bbd univercity";
+   return h1;
+}
+function myLength(){
+    let text="i am \'harendra prajapati\' and i am mca student at bbd univercity";
+    return text.length;
+}
+
+function myString1(){
+    let text="i am \'harendra prajapati \' i am mca student at bbd univercity";
+    return text;
+}
+
+function myString2(){
+    let text="i am \"harendra prajapati \" i am mca student at bbd univercity";
+    return text;
+}
+function myString3(){
+    let text="i am harendra prajapati \\ i am mca student at bbd univercity";
+    return text;
+}
+
+function myFunction(){
+    let text="i am harendra prajapati \b i am mca student at bbd univercity";
+    return text;
+}
+function myFunction1(){
+    let text="i am harendra prajapati \f i am mca student at bbd univercity";
+    return text;
+}
+function myFunction2(){
+    let text="i am harendra prajapati \n i am mca student at bbd univercity";
+    return text;
+}
+function myFunction3(){
+    let text="i am harendra prajapati \t i am mca student at bbd univercity";
+    return text;
+}
+function myFunction4(){
+    let text="i am harendra prajapati \r i am mca student at bbd univercity";
+    return text;
+}
+function myFunction5(){
+    let text="i am harendra prajapati \v i am mca student at bbd univercity";
+    return text;
+}
+function myFunction6(){
+    let text="i am harendra prajapati \ i am mca student at bbd univercity";
+    return text;
+}
+function myFunction7(){
+    let text="i am harendra prajapati ! i am mca student at bbd univercity";
+    return text;
+}
+
+function newString(){
+    let x = "John";
+let y = new String("John");
+ return typeof x;
+ return typeof y;
+    
+}
+
+function newString1(){
+    let x = "John";
+let y = new String("John");
+
+ return typeof y;
+    
+}
+function myFunction_h0(){
+    let h1="harendra";
+    let h2="harendra";
+  return (h1==h2);
+}
+
+function myFunction_h1(){
+    let h1="harendra";
+    let h2=newString("harendra");
+  return (h1===h2);
+}
+
+
+
+
+function myFunction_h2(){
+    let h1="harendra";
+    let h2=newString("harendra");
+  return (h1==h2);
+}
+
+
+function myFunction_h3(){
+    let h1=newString("harendra");
+    let h2=newString("harendra");
+  return (h1==h2);
+}
+
+
+function myFunction_h4(){
+    let h1="harendra";
+    let h2=newString("harendra");
+  return (h1==h2);
+}
+
+
