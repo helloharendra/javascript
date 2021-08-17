2207004679fa679d6988cfe54b509b55377f4c6a
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-08-18 00:32:49 +0530

    JsStringMathods

---
+++ JsStringMathod.js
@@ -0,0 +1,63 @@
+function myString(){
+    str1='Shubham Harendra Ankit';
+   return("the string is    :"+str1);
+
+    
+}
+
+function myLength(){
+    let str1='Shubham Harendra Ankit';
+    return ("the string lenth is:"+str1.length);
+    
+}
+
+
+function mySlice0(){
+
+    str1='Shubham Harendra Ankit';
+   return ("the slice of string is    :"+str1.slice(7,16));
+}
+
+function mySlice1(){
+
+    str1='Shubham Harendra Ankit';
+   return ("the string reverse slice is    :"+str1.slice(-14,-5));
+}
+function mySlice2(){
+
+    str1='Shubham Harendra Ankit';
+   return ("the slice is from index 0 is    : "+str1.slice(0));
+}
+function mySlice3(){
+
+    str1='Shubham Harendra Ankit';
+   return ("the slice is from index 7 is    : "+str1.slice(0));
+}
+function mySlice4(){
+
+    str1='Shubham Harendra Ankit';
+   return ("the slice is from index 16 is    : "+str1.slice(16));
+}
+function mySlice5(){
+
+    str1='Shubham Harendra Ankit';
+   return ("the slice is from index -16 is    : "+str1.slice(-14));
+}
+
+function mySubstring(){
+    str1='Shubham Harendra Ankit';
+    return str1.substr(8,9);
+}
+function mySubstring1(){
+    str1='Shubham Harendra Ankit';
+    return str1.substr(7);
+}
+function mySubstring2(){
+    str1='Shubham Harendra Ankit';
+    return str1.substr(-6);
+}
+
+function myReplace1() {
+    let text1="this is harendra!";
+    let newText=text1.replace("harendra" , "Harry") ;
+  }
