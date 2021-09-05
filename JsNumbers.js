889d86f702b59691734397a9ca0cc8e99854a57a
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-09-06 00:20:08 +0530

    Js Numbers

---
+++ JsNumbers.js
@@ -0,0 +1,90 @@
+function myFunction(){
+    let x = 10;
+let y = 20;
+let z = x + y;           
+    return z;
+}
+function myFunction1(){
+    let x = 10;
+    let y = 20;
+    let z = "The result is: " + x + y;
+    return z;
+}
+function myFunction2(){
+    let x = "10";
+let y = "20";
+let z = x + y;        
+    return z;
+}function myFunction3(){
+    let x = 10;
+let y = "20";
+let z = x + y;   
+    return z;
+}function myFunction4(){
+    let x = "10";
+let y = 20;
+let z = x + y;  
+}function myFunction5(){
+    let x = 10;
+let y = 20;
+let z = "The result is: " + x + y;
+    return z;
+}function myFunction6(){
+    let x = "100";
+let y = "10";
+let z = x / y;       // z will be 10
+    return z;
+}function myFunction7(){
+    let x = "100";
+let y = "10";
+let z = x * y;
+return z;
+}function myFunction8(){
+    let x = "100";
+let y = "10";
+let z = x - y;       // z will be 90
+    return z;
+}
+
+
+function myFunction9(){
+    let x = "100";
+let y = "10";
+let z = x + y;       // z will not be 110 (It will be 10010)
+    return z;
+}
+
+function myFunction10(){
+    let myNumber = 2; 
+    let txt = " ";
+    while (myNumber != Infinity) 
+    {
+       myNumber = myNumber * myNumber;
+       txt = txt + myNumber + "<br>";
+    }
+    return txt;
+}
+
+function myFunction11(){
+    let myNumber = 32;
+let z ="32 = " + "<br>" + 
+" Decimal " + myNumber.toString(10) + "<br>" +
+" Hexadecimal " + myNumber.toString(16) + "<br>" +
+" Octal " + myNumber.toString(8) + "<br>" +
+" Binary " + myNumber.toString(2);
+    return z;
+}
+
+function myFunction8(){
+    let x = "100";
+let y = "10";
+let z = x - y;       // z will be 90
+    return z;
+}
+
+function myFunction8(){
+    let x = "100";
+let y = "10";
+let z = x - y;       // z will be 90
+    return z;
+}
