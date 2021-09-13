08e467619cbe18bd064b80fbaf24f94ba5c5d4d1
Harendra Prajapati <harendraprajapati72@gmail.com>
2021-09-13 23:37:32 +0530

    Js Array

---
+++ JsArray.js
@@ -0,0 +1,52 @@
+function myFunction(){
+    const cars=['bmw','rolls roys ','audy'];
+return cars;
+}
+
+function myFunction1(){
+    const cars=[
+        'bmw',
+    'rolls roys ',
+    'audy'
+];
+return cars;
+}
+
+function myFunction2(){
+    const cars=[];
+    cars[0]='bmw';
+    cars[1]='rolls roys ';
+    cars[2]='audy';
+return cars;
+}
+
+function myFunction3(){
+    const cars = new Array("Saab", "Volvo", "BMW");
+return cars;
+}
+
+function myFunction4(){
+    const cars = new Array("Saab", "Volvo", "BMW");
+    let x=cars[0];
+return x;
+}
+
+function myFunction5(){
+    const cars = new Array("Saab", "Volvo", "BMW");
+     cars[0]='rolls roys';
+return cars;
+}
+
+function myFunction6(){
+    const cars = new Array("Saab", "Volvo", "BMW");
+     
+return cars[0];
+}
+
+function myFunction6(){
+    const person = {firstName:"John", lastName:"Doe", age:46};
+     
+return person.firstName;
+}
+
+
