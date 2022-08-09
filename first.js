276f2024edfb9d56341d87c4d22baa95fcfd6160
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-08-10 03:30:13 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,29 @@
+function myFunction(){
+    window.alert("Thank you @harendraprajapati72")
+}
+function myFunction1(){
+    a=10;
+    b=20;
+    c=a+b;
+    document.write("Addition is : "+c);
+}
+
+function myFunction2(){
+    a=10;
+    b=20;
+    c=a-b;
+    document.write("Substraction is : "+c);
+}
+function myFunction3(){
+    a=10;
+    b=20;
+    c=a*b;
+    document.write("Multiplication is : "+c);
+}
+
+function myFunction4(){
+    a=10;
+    b=20;
+    c=a/b;
+    document.write("Division is : "+c);
+}
